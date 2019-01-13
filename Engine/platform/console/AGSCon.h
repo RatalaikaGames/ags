6d85d87449e86da969c1b6627dc1945d0f427f42
mgambrell <mgambrell@users.noreply.github.com>
2019-01-12 19:09:32 -0500

    add some new AGSCON apis

--- Engine/platform/console/AGSCon.h
+++ Engine/platform/console/AGSCon.h
@@ -12,6 +12,7 @@ namespace AGSCON
 		class Program;
 		class RenderTarget;
 		class VertexLayout;
+		class Sampler;
 
 		//the format of a vertex element (aka attribute)
 		enum class AttributeFormat
@@ -35,6 +36,37 @@ namespace AGSCON
 			const VertexLayoutDescr_Attribute* Attributes;
 		};
 
+		enum class SamplerMinFilter
+		{
+			Nearest,
+			Linear
+		};
+
+		enum class SamplerMagFilter
+		{
+			Nearest,
+			Linear
+		};
+
+		enum class SamplerWrapMode
+		{
+			Clamp,
+			Repeat
+		};
+
+		struct SamplerDescr
+		{
+			SamplerMinFilter MinFilter;
+			SamplerMagFilter MagFilter;
+			SamplerWrapMode WrapModeS;
+			SamplerWrapMode WrapModeT;
+		};
+
+		void BeginFrame();
+		void BeginRender();
+		void EndRender();
+		void EndFrame();
+
 		Texture* Texture_Create(int w, int h);
 		void Texture_Destroy(Texture* tex);
 
@@ -47,11 +79,26 @@ namespace AGSCON
 		RenderTarget* RenderTarget_Create(int width, int height);
 		void RenderTarget_Destroy(RenderTarget* rt);
 
+		Sampler* Sampler_Create(const SamplerDescr* samplerDescr);
+		void Sampler_Destroy(Sampler* sampler);
+
 		void SetViewport(int x, int y, int w, int h);
 		void SetScissor(int x, int y, int w, int h);
 
 		//sets the scissor to the last-set viewport
 		//would be better to not lose track of the viewport (need to update this driver to do that)
 		void ClearScissor();
+
+		//Sets the current render target
+		//AND sets the viewport to match it
+		//AND clears the scissor
+		void SetRenderTarget(int index, RenderTarget* rt);
+
+		void SetBackbufferRenderTarget();
+		
+		//Clears the currently set RT's color buffer only
+		void ClearColor(int red, int green, int blue, int alpha);
+
+		void BindFragmentTexture(int target, Texture* texture, Sampler* sampler);
 	}
 }

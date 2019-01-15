566a4a5634e3b0b2fe255860b3ea897ec24719dc
mgambrell <mgambrell@users.noreply.github.com>
2019-01-15 04:48:09 -0500

    add a build compatibility fix for the included ogg headers. I dont really know what the hell is going on here. I dont really think it's a good idea to be including these, unless they work, so... this helps them work.

--- Common/libinclude/OGG/config_types.h
+++ Common/libinclude/OGG/config_types.h
@@ -1,8 +1,13 @@
 #ifndef __CONFIG_TYPES_H__
 #define __CONFIG_TYPES_H__
 
+#ifdef CONSOLE_VERSION
+#include "platform/console/AGSCon_types.h"
+#endif
+
 /* these are filled in by configure */
 typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
 typedef int32_t ogg_int32_t;
 typedef u_int32_t ogg_uint32_t;
 typedef int64_t ogg_int64_t;
---
+++ Engine/platform/console/AGSCon_types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdint.h>
+
+//wtf
+typedef uint32_t u_int32_t;
